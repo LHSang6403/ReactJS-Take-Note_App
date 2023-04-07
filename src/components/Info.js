6a2e1fc0dc8dc0f6cd1f39240bf04c7dd5e7c547
Xang <88763282+LHSang6403@users.noreply.github.com>
2023-04-07 10:53:57 +0700

    add hover animation

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,14 +4,21 @@ function Info(){
     return (
         <div className='todo-app shadow' >
             <h2>React Note App</h2>
-            <div>
+            <div className="img-wrap">
                 <img src={myImage} alt="" className="img-avt"/>
             </div>
-            <p>
-                <h1>Student: Hoang-Sang Le</h1>
-                <h1>HCMUS - Computer Science</h1>
-                <h1>lhsang64.contact@gmail.com</h1>
-            </p>
+            <ul className="info-content">
+                <p className="info-name info-content">
+                    <li >Student: Hoang-Sang Le</li>
+                </p>
+                <p className="info-uni info-content">
+                    <li >HCMUS - Computer Science</li>
+                </p>
+                <p className="info-email info-content">
+                    <li >lhsang64.contact@gmail.com</li>
+                </p>
+            </ul>
+                
         </div>
     )
 }
